501c7200eb857864ec2e6ed07115594a47c2be73
mqchen <mq.chen@gmail.com>
2011-12-11 02:51:31 +0100

    Removed project specific file

--- static_files_compressor/localdeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Desginstudier
-cp -rv * ~/Dropbox/Documents/Newzoo/Projects/DesignStudier/Site/cms/extensions/static_files_compressor/
-
-# Follo
-cp -rv * ~/Dropbox/Documents/Newzoo/Projects/Follo/Development/Forum/extensions/static_files_compressor/
